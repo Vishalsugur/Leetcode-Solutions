bec3f60452aa63c1bd00b33a4b78242c4f5a842f
Vishalsugur <104304607+Vishalsugur@users.noreply.github.com>
2023-02-04 11:44:57 +0530

    Create find peak element.py

---
+++ find peak element.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l = [*set(nums)]
+        l.sort()
+        for i in range(len(nums)):
+            if l[-1]==nums[i]:
+                
+                return i
